8765e4c4fa20bdb017596e6e3b5751f4fdaab85c
Mahesh2204 <113750079+Mahesh2204@users.noreply.github.com>
2024-03-05 09:54:37 +0530

    Update DriverSetup.java

--- src/main/java/Resources/DriverSetup.java
+++ src/main/java/Resources/DriverSetup.java
@@ -16,30 +16,33 @@ public class DriverSetup {
 
 	public WebDriver SelectDriver() throws MalformedURLException {
 		WebDriver d = null;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter \n1 for remote webdriver\n2 for local webdriver");
-		int x = sc.nextInt();
-		if (x == 1) {
-			DesiredCapabilities ds = new DesiredCapabilities();
-			//ds.setCapability("browserName", "chrome");
-			ds.setBrowserName("chrome");
-			//ds.setBrowserName("MicrosoftEdge");
+		DesiredCapabilities ds = new DesiredCapabilities();
+		 ds.setBrowserName("chrome");
+		d = new RemoteWebDriver(new URL(huburl), ds);
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("Enter \n1 for remote webdriver\n2 for local webdriver");
+		// int x = sc.nextInt();
+		// if (x == 1) {
+		// 	DesiredCapabilities ds = new DesiredCapabilities();
+		// 	//ds.setCapability("browserName", "chrome");
+		// 	ds.setBrowserName("chrome");
+		// 	//ds.setBrowserName("MicrosoftEdge");
 
-			d = new RemoteWebDriver(new URL(huburl), ds);
-		} else if (x == 2) {
-			System.out.println("Enter the browser to execute \n1.Chrome\n2.Edge");
-			int a = sc.nextInt();
-			switch (a) {
-			case 1:
-				d = new ChromeDriver();
-				break;
-			case 2:
-				d = new EdgeDriver();
-				break;
-			default:
-				System.out.println("Wrong choice\n");
-			}
-		}
+		// 	d = new RemoteWebDriver(new URL(huburl), ds);
+		// } else if (x == 2) {
+		// 	System.out.println("Enter the browser to execute \n1.Chrome\n2.Edge");
+		// 	int a = sc.nextInt();
+		// 	switch (a) {
+		// 	case 1:
+		// 		d = new ChromeDriver();
+		// 		break;
+		// 	case 2:
+		// 		d = new EdgeDriver();
+		// 		break;
+		// 	default:
+		// 		System.out.println("Wrong choice\n");
+		// 	}
+		// }
 		return d;
 	}
 
